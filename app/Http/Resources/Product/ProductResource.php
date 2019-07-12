11c0676c2aa30ed150a20b38a0568fc8c136d7e9
iantoom <radityo.perwianto@gmail.com>
2019-07-12 13:55:18 +0700

    add calculated payloads to ProductResource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,15 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round($this->price*(1-$this->discount/100), 2),
+            'rating' => $this->reviews->count() > 0 ?
+                \round($this->reviews->sum('star')/$this->reviews->count(), 2, PHP_ROUND_HALF_UP) // limit the digits after decimal point and round the decimal point half up
+                : 'No review for this product',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id) // return link in JSON payload
+            ]
         ];
     }
 }
