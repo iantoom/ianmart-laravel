80c2d46d6f9a7cdc0bb19fb0f14e8ac59f4039d4
iantoom <radityo.perwianto@gmail.com>
2019-07-12 15:14:27 +0700

    Create Custom Payload for ProductCollection

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -3,8 +3,9 @@
 namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +15,18 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round($this->price*(1-$this->discount/100), 2),
+            'rating' => $this->reviews->count() > 0 ?
+                \round($this->reviews->sum('star')/$this->reviews->count(), 2, PHP_ROUND_HALF_UP) // limit the digits after decimal point and round the decimal point half up
+                : 'No review for this product',
+            'href' => [
+                'detail' => route('products.show', $this->id) // return link in JSON payload
+            ]
+        ];
     }
 }
